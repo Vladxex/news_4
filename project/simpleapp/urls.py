75746df65701c08c2a4455f946fbabd6c85949f1
Vladislav <vlad_19_96@mail.ru>
2021-01-19 20:58:04 +0300

    create->add/edit.?

--- project/simpleapp/urls.py
+++ project/simpleapp/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', News.as_view()),
     # т.к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
     path('<int:pk>', NewDetail.as_view(), name='new_detail'),  # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
-    path('create/', NewCreateView.as_view(), name='new_create'),  # Ссылка на create new
-    path('delete/<int:pk>/', NewDeleteView.as_view(), name='new_delete'),  # Ссылка на delete new
-    path('create/<int:pk>/', NewUpdateView.as_view(), name='new_update'),  # Ссылка на update new
+    path('add/', NewCreateView.as_view(), name='new_create'),  # Ссылка на create new
+    path('<int:pk>/delete/', NewDeleteView.as_view(), name='new_delete'),  # Ссылка на delete new
+    path('<int:pk>/edit/', NewUpdateView.as_view(), name='new_update'),  # Ссылка на update new
 
 ]
