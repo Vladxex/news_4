fd07fa498af2e4feeb6d1d318de2353f33c0c2ac
Vladislav <vlad_19_96@mail.ru>
2021-01-19 20:17:18 +0300

    ^2d8

--- project/simpleapp/models.py
+++ project/simpleapp/models.py
@@ -9,17 +9,12 @@ class New(models.Model):
         unique=True,  # названия новостей не должны повторяться
     )
     description = models.TextField()
-    quantity = models.IntegerField(
-        validators=[MinValueValidator(0)],
-    )
+    quantity = models.IntegerField(validators=[MinValueValidator(0)],)
     # поле категории будет ссылаться на модель категории
     category = models.ForeignKey(
         to='Category',
         on_delete=models.CASCADE,
         related_name='news',  # все новости в категории будут доступны через поле news
-    )
-    price = models.FloatField(
-        validators=[MinValueValidator(0.0)],
     )
     auto_time = models.DateTimeField(auto_now_add=True)
 
--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -1,54 +1,43 @@
 # from django.shortcuts import render
-from django.views.generic import ListView, UpdateView, CreateView, DetailView, DeleteView  # импортируем класс получения деталей объекта
+from django.views.generic import ListView, UpdateView, CreateView, DetailView, \
+    DeleteView  # импортируем класс получения деталей объекта
 from .models import New
-from django.core.paginator import Paginator # импортируем класс, позволяющий удобно осуществлять постраничный вывод
+from django.core.paginator import Paginator  # импортируем класс, позволяющий удобно осуществлять постраничный вывод
 from django.shortcuts import render
 from .filters import NewFilter
 from .forms import NewForm
 
-class NewsList(ListView):
-    model = New
-    template_name = 'news.html'
-    context_object_name = 'news'
+
+class News(ListView):
     paginate_by = 10  # поставим постраничный вывод в ten elements
-    queryset = New.objects.order_by('-auto_time')
+    model = New
+    template_name = 'new_list.html'
+    context_object_name = 'news'
+    ordering = ['-auto_time']
 
     # метод get_context_data нужен нам для того, чтобы мы могли передать переменные в шаблон. В возвращаемом словаре context будут храниться все переменные. Ключи этого словари и есть переменные, к которым мы сможем потом обратиться через шаблон
 
-    def get_context_data(self, **kwargs): # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса (привет полиморфизм, мы скучали!!!)
+    def get_context_data(self, **kwargs):  # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса (привет полиморфизм, мы скучали!!!)
         context = super().get_context_data(**kwargs)
-        context['filter'] = NewFilter(self.request.GET, queryset=self.get_queryset()) # вписываем наш фильтр в контекст
+        context['filter'] = NewFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
         return context
-
-
-    def get(self, request):
-        news = New.objects.order_by('-auto_time')
-        p = Paginator(news, 10)  # создаём объект класса пагинатор, передаём ему список наших товаров и их количество для одной страницы
-
-        news = p.get_page(request.GET.get('page', 1))  # берём номер страницы из get-запроса. Если ничего не передали, будем показывать первую страницу.
-        # теперь вместо всех объектах в списке товаров хранится только нужная нам страница с товарами
-
-        data = {
-            'news': news,
-        }
-        return render(request, 'new_list.html', data)
 
 
 # создаём представление в котором будет детали конкретного отдельного товара
 class NewDetail(DetailView):
-    model = New  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
-    template_name = 'new.html'  # название шаблона будет new.html
-    context_object_name = 'new'  # название объекта
+    template_name = 'simpleapp/new_detail.html'
+    queryset = New.objects.all()
+
 
 # дженерик для создания объекта. Надо указать только имя шаблона и класс формы который мы написали в прошлом юните. Остальное он сделает за вас
 class NewCreateView(CreateView):
-    template_name = 'sample_app/new_create.html'
+    template_name = 'simpleapp/new_create.html'
     form_class = NewForm
 
 
 # дженерик для редактирования объекта
 class NewUpdateView(UpdateView):
-    template_name = 'sample_app/new_create.html'
+    template_name = 'simpleapp/new_create.html'
     form_class = NewForm
 
     # метод get_object мы используем вместо queryset, чтобы получить информацию об объекте который мы собираемся редактировать
@@ -59,7 +48,6 @@ class NewUpdateView(UpdateView):
 
 # дженерик для удаления товара
 class NewDeleteView(DeleteView):
-    template_name = 'sample_app/new_delete.html'
+    template_name = 'simpleapp/new_delete.html'
     queryset = New.objects.all()
     success_url = '/news/'
-
